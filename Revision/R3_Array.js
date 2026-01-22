64ce3aa0ed64533aacb70c1bbb4c968523d5e6bf
Anshumaankhare2403 <anshumaankhare2403@gmail.com>
2026-01-22 18:59:58 +0530

    DSA

--- Revision/R3_Array.js
+++ Revision/R3_Array.js
@@ -52,3 +52,28 @@ console.log(fullSongArray);
 
 let data = fullSongArray.slice(2);
 console.log(data)
+
+
+let newArr = [1,2,3,4,5,6,7,8,9,10];
+let out = newArr.indexOf(10);
+console.log(out)// it show the element is present inside the array or not 
+
+let newOut = newArr.includes(10);
+console.log(newOut)
+
+let arr1 = [1,2,3,4];
+let arr2 = [5,6,7,8];
+const newARR=arr1.concat(arr2);
+const ARR = arr1+arr2;//not good way to concat Two array 
+console.log(newARR);
+console.log(ARR);
+
+
+let dataARR = [9,3,2,4,5,1,6,7,8,99];
+console.log(dataARR.sort((a,b)=>a-b));
+
+
+let datass = [...arr1]
+console.log(datass)
+console.log(dataARR===arr1)// Values are same, but references are different 
+//Spread operator creates a shallow copy
--- Revision/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-while(i<n){
-//     console.log(arr[i])
-//     i++;
-// }
