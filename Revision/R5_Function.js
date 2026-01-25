497ab3956d4520b57f9dabfc42e10c608beb1bd0
Anshumaankhare2403 <anshumaankhare2403@gmail.com>
2026-01-25 17:58:35 +0530

    DSA

---
+++ Revision/R5_Function.js
@@ -0,0 +1,16 @@
+const evenOdd = (num)=>{
+    if(num%2==0){
+        console.log("even num");
+    }
+    else{
+        console.log("odd num");
+    }
+}
+
+evenOdd(1);
+
+const areaofacircul = (r)=>{
+    let area = 22/7*r*r
+    return area;
+}
+console.log(areaofacircul(10))
