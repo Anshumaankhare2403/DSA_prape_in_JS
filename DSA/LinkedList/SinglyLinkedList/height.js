f7e32359e308ff92eb5248a92ebd096082384f79
Anshumaankhare2403 <anshumaankhare2403@gmail.com>
2026-02-12 10:57:32 +0530

    dsa

---
+++ DSA/LinkedList/SinglyLinkedList/height.js
@@ -0,0 +1,13 @@
+class Node{
+    constructor(data){
+        this.data = data
+        this.left = null ;
+        this.rigth= null ;
+    }
+}
+
+class Balanced{
+    isblaanced(){
+        return this.chek
+    }
+}
---
+++ DSA/Trees/BinaryTree/BT/tempCodeRunnerFile.js
@@ -0,0 +1,63 @@
+class node {
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null; 
+    }
+}
+
+// now next should be how to implement insertion in tree
+
+class BT{
+    constructor(){
+        this.root = null;
+    }
+
+    insertion(data){
+    const nn = new Node(data);
+
+    if(this.root === null){
+        this.root = nn;
+        return;  // VERY IMPORTANT
+    }
+
+    let current = this.root;
+
+    while(current !== null){
+
+        if(data < current.data){
+
+            if(current.left === null){
+                current.left = nn;
+                return;
+            }
+
+            current = current.left;
+
+        } else {
+
+            if(current.right === null){
+                current.right = nn;
+                return;
+            }
+
+            current = current.right;
+        }
+    }
+}
+
+    inorder(node= this.root){
+        if (!node) return;
+        this.inorder(node.left);
+        console.log(node.data);
+        this.inorder(node.right);
+    }
+}
+
+
+const dt1 = new BT();
+dt1.insertion(10);
+dt1.insertion(11);
+dt1.insertion(12);
+
+dt1.inorder();
